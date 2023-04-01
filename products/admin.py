5f22bc7d4b695fa4e2215d1a0c1369c6229288dd
mahdibagheri <bagheri.almas@gmail.com>
2023-04-01 22:37:40 +0330

    add models to admin Panel: - Size - Category - Specification - Product - File - Banner

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Product, Category, Size, File, Specification
+
 
 # Register your models here.
+
+
+class FileAdminInline(admin.TabularInline):
+    model = File
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [FileAdminInline, ]
+    filter_horizontal = ('sizes', 'specification')
+
+
+admin.site.register(Size)
+admin.site.register(Product, ProductAdmin)
+
+
+class ParentCategoryFilter(admin.SimpleListFilter):
+    title = 'Parent Category'
+    parameter_name = 'parent'
+
+    def lookups(self, request, model_admin):
+        categories = Category.objects.filter(parent__isnull=True)
+        return [(c.id, str(c)) for c in categories]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(parent_id=self.value())
+        else:
+            return queryset
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_filter = (ParentCategoryFilter,)
+    search_fields = ('parent__title',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Specification)

5ae91309a02bf3d52381c75c4a6e86f01fb15915
mahdibagheri <bagheri.almas@gmail.com>
2023-04-01 15:25:55 +0330

    create urls.py in users app and link app urls to main urls.py

--- OnlineShop/urls.py
+++ OnlineShop/urls.py
@@ -1,6 +1,17 @@
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+from OnlineShop import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('accounts/', include('users.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='pages/home.html'), name='home')
 ]
+
+if settings.IS_DEVELOPING:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

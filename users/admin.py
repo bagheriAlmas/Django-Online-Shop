f94f6b1179738bdd4f392492b5a5f3d78274222f
mahdibagheri <bagheri.almas@gmail.com>
2023-04-01 15:23:05 +0330

    add user model to admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.forms import CustomUserCreationForm, CustomUserChangeForm
+from users.models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm  # dokmeye add kardan yek user
+    form = CustomUserChangeForm  # formi ke haminjuri aval be ma neshun mide
+    model = CustomUser
+    list_display = ['username', 'email', 'phone', ]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('gender', 'phone', 'avatar', 'postal_code', 'address'), }),)  # baraye ChangeForm
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('gender', 'phone', 'avatar', 'postal_code', 'address'), }),)  # baraye CreationForm
+
+
+admin.site.register(CustomUser, CustomUserAdmin)

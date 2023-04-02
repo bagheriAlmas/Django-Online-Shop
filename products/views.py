c23c99963aa9b089a8705e50940e30b78166db4e
mahdibagheri <bagheri.almas@gmail.com>
2023-04-02 21:09:16 +0330

    merge _banner, _categories-of-month.html, _feature-product.html to home.html

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import Banner
+
+
+def banner_list_view(request):
+    banners = Banner.objects.all().filter(is_enabled=True)
+    context = {
+        'banners': banners,
+    }
+    return render(request, 'pages/home.html', context)

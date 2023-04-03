1517201950b9035d29d9a67021122e9701fa3987
mahdibagheri <bagheri.almas@gmail.com>
2023-04-03 16:42:04 +0330

    add featured list to data_list_view

--- products/views.py
+++ products/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
-from products.models import Banner, Category
+from products.models import Banner, Category, Product
 
 
 def data_list_view(request):
     banners = Banner.objects.all().filter(is_enabled=True)
     categories = Category.objects.all().filter(highlight=True)
+    featured = Product.objects.all().filter(featured=True)
     context = {
         'banners': banners,
         'categories':categories,
+        'featured' : featured,
     }
     return render(request, 'pages/home.html', context)

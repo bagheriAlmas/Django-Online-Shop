47fb0ead9b6200ce652b15050252e03cb8d53118
mahdibagheri <bagheri.almas@gmail.com>
2023-04-04 00:08:14 +0330

    create product_detail_view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from products.models import Banner, Category, Product
 
@@ -9,7 +9,17 @@ def data_list_view(request):
     featured = Product.objects.all().filter(featured=True)
     context = {
         'banners': banners,
-        'categories':categories,
-        'featured' : featured,
+        'categories': categories,
+        'featured': featured,
     }
     return render(request, 'pages/home.html', context)
+
+
+def product_detail_view(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    related_products = Product.objects.all().filter(featured=False)[:10]
+    context = {
+        'product': product,
+        'related_products': related_products,
+    }
+    return render(request, 'pages/product_detail.html', context)

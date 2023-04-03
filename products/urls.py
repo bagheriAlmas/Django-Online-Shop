fe333a456f53fd6e80e1aa95619a9a1b391b18ec
mahdibagheri <bagheri.almas@gmail.com>
2023-04-03 23:41:17 +0330

    add product_detail url

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import data_list_view
+from .views import data_list_view, product_detail_view
+
 urlpatterns = [
-    path('',data_list_view,name='banner')
+    path('', data_list_view, name='banner'),
+    path('product/<slug:slug>/', product_detail_view, name='product_detail')
 ]

18678ccc9b80da532fb72879de634b1a2af48ddd
mahdibagheri <bagheri.almas@gmail.com>
2023-04-04 18:51:46 +0330

    create contact-us views and form and use it in contact.html

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from contact_us.forms import ContactUsForm
+
+
+def contact_us_view(request):
+    form = ContactUsForm()
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            data = {
+                'name': form.data['name'],
+                'email': form.data['email'],
+            }
+            return render(request, 'pages/contact_complete.html', {'data': data})
+
+    else:
+        form = ContactUsForm()
+    return render(request, 'pages/contact.html', {'form': form})

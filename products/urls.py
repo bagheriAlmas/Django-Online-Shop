a622d0bfe00b362d45dffc8f7c348f41dc3b2039
mahdibagheri <bagheri.almas@gmail.com>
2023-04-04 11:23:22 +0330

    add urls for about.html, contact.html, shop.html

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,14 @@
+from django.shortcuts import render
 from django.urls import path
+from django.views.generic import TemplateView
+
 from .views import data_list_view, product_detail_view
 
 urlpatterns = [
     path('', data_list_view, name='banner'),
-    path('product/<slug:slug>/', product_detail_view, name='product_detail')
+    path('product/<slug:slug>/', product_detail_view, name='product_detail'),
+    path('about/', lambda request: render(request, 'pages/about.html'), name='about-us'),
+    path('shop/', lambda request: render(request, 'pages/shop.html'), name='shop'),
+    path('contact/', lambda request: render(request, 'pages/contact.html'), name='contact-us'),
+
 ]

22447cc621659e5e615df06d340a6f59b66284cb
mahdibagheri <bagheri.almas@gmail.com>
2023-04-05 20:09:16 +0330

    update path of category_list_view

--- products/urls.py
+++ products/urls.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import data_list_view, product_detail_view
+from .views import data_list_view, product_detail_view, category_list_view
 
 urlpatterns = [
     path('', data_list_view, name='banner'),
     path('product/<slug:slug>/', product_detail_view, name='product_detail'),
     path('about/', lambda request: render(request, 'pages/about.html'), name='about-us'),
-    path('shop/', lambda request: render(request, 'pages/shop.html'), name='shop'),
+    path('shop/', category_list_view, name='shop'),
 ]

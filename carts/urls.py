bded2f394d5e3916d50ef89f40565f799ad7b3f5
mahdibagheri <bagheri.almas@gmail.com>
2023-04-07 12:22:46 +0330

    define user_cart_list_view path in urls

--- carts/urls.py
+++ carts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import user_cart_view,create_cart_view
+from .views import user_cart_list_view, create_cart_view
+
 urlpatterns = [
-    path('<int:pk>/cart/',user_cart_view,name='cart'),
-    path('create/',create_cart_view,name='create-cart'),
+    path('<int:pk>/cart/', user_cart_list_view, name='cart'),
+    path('create/', create_cart_view, name='create-cart'),
 ]

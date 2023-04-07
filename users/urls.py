9ea6027007cc5b11e01dad82c656d21eb93ccddc
mahdibagheri <bagheri.almas@gmail.com>
2023-04-07 19:01:23 +0330

    add path for user detail view

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, user_detail_view
+
 urlpatterns = [
-    path('',UserRegisterView.as_view(),name='register')
+    path('', UserRegisterView.as_view(), name='register'),
+    path('profile', user_detail_view, name='user_profile')
 ]
